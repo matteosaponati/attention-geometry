57a49fbfda1a51f854277a5994f1a88d9335fdf8
matteosaponati <matteosaponati@gmail.com>
2024-02-20 15:02:25 +0100

    utils: add funs.py with fun for calculating symmetry scores - every notebook calls the same function

---
+++ utils/funs.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+def scores(A):
+    """
+    Given a square matrix A, calculate the symmetric (S) and skew-symmetric (N) scores of a matrix.
+    Args:
+        - A (numpy.ndarray) : square numpy matrix.
+
+    Returns:
+        - tuple : Symmetric (S) and skew-symmetric (N) scores.
+    """
+     
+    S = np.linalg.norm(.5 * (A + A.T), 'fro') / np.linalg.norm(A, 'fro')
+    N = np.linalg.norm(.5 * (A - A.T), 'fro') / np.linalg.norm(A, 'fro')
+
+    return S, N
