547714292a6b90819ebbe341ffa6d583b7bb0ea8
matteosaponati <matteosaponati@gmail.com>
2024-02-29 10:47:21 +0100

    add funs to compute dot products of vectors from normal ball

--- utils/funs.py
+++ utils/funs.py
@@ -14,3 +14,21 @@ def scores(A):
     N = np.linalg.norm(.5 * (A - A.T), 'fro') / np.linalg.norm(A, 'fro')
 
     return S, N
+
+def dotproduct_normal(A,rep=100):
+    """
+    Given a square matrix A, calculate the symmetric (S) and skew-symmetric (N) scores of a matrix.
+    Args:
+        - A (numpy.ndarray) : square numpy matrix.
+
+    Returns:
+        - tuple : Symmetric (S) and skew-symmetric (N) scores.
+    """
+
+    u = np.random.randn(rep,A.shape[0])
+    v = np.random.randn(rep,A.shape[0]) 
+
+    Dots = u @ A @ v.T
+    DotsCanonical = u @ v.T
+
+    return Dots, DotsCanonical
