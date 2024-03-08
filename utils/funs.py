fc4064218fd3b9838b484aeed9e7660fcf36dba8
matteosaponati <matteosaponati@gmail.com>
2024-03-08 10:14:32 +0100

    funs: add fun to calculate distances between random vectors

--- utils/funs.py
+++ utils/funs.py
@@ -25,10 +25,34 @@ def dotproduct_normal(A,rep=100):
         - tuple : Symmetric (S) and skew-symmetric (N) scores.
     """
 
-    u = np.random.randn(rep,A.shape[0])
-    v = np.random.randn(rep,A.shape[0]) 
+    u = np.random.rand(rep,A.shape[0])
+    v = np.random.rand(rep,A.shape[0]) 
 
-    Dots = u @ A @ v.T
-    DotsCanonical = u @ v.T
+    dot = np.zeros(rep)
+    dotC = np.zeros(rep)
 
-    return Dots, DotsCanonical
+    for k in range(rep):
+        dot[k] = np.dot(u[k,:], A @ v[k,:].T) / (np.linalg.norm(u[k,:]) * np.linalg.norm(A @ v[k,:].T))        
+        dotC[k] = np.dot(u[k,:],v[k,:]) / (np.linalg.norm(u[k,:]) * np.linalg.norm(v[k,:]))
+
+    return dot,dotC
+
+def isotropic_normal(A,rep=100):
+    """
+    Given a square matrix A, calculate the symmetric (S) and skew-symmetric (N) scores of a matrix.
+    Args:
+        - A (numpy.ndarray) : square numpy matrix.
+
+    Returns:
+        - tuple : Symmetric (S) and skew-symmetric (N) scores.
+    """
+
+    u = np.random.rand(rep,A.shape[0])
+    dot = np.zeros(rep)
+    dotC = np.zeros(rep)
+    
+    for k in range(rep):
+        dot[k] = np.dot(u[k,:], A @ u[k,:].T) / (np.linalg.norm(u[k,:]) * np.linalg.norm(A @ u[k,:].T))        
+        dotC[k] = np.dot(u[k,:],u[k,:]) / (np.linalg.norm(u[k,:])**2)
+
+    return dot,dotC
