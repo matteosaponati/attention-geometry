8d8aa32b1f6f517837470c3e89f815451220b592
matteosaponati <matteosaponati@gmail.com>
2024-05-02 16:56:26 +0200

    add fun to compute norm and trace(M@M) efficiently

--- utils/funs.py
+++ utils/funs.py
@@ -26,44 +26,17 @@ def scores_trace(Wq, Wk):
 
     return S
 
-def dotproduct_normal(A,rep=100):
-    """
-    Given a square matrix A, calculate the symmetric (S) and skew-symmetric (N) scores of a matrix.
-    Args:
-        - A (numpy.ndarray) : square numpy matrix.
+def zetas(Wq, Wk):
+     
+    M = Wq.T @ Wk
+    idx = np.einsum('ij,ji->', M, M) 
 
-    Returns:
-        - tuple : Symmetric (S) and skew-symmetric (N) scores.
-    """
+    return idx
 
-    u = np.random.rand(rep,A.shape[0])
-    v = np.random.rand(rep,A.shape[0]) 
+def frobenious_norm(Wq, Wk):
+     
+    A = Wq.T @ Wq
+    B = Wk.T @ Wk
+    norm = np.einsum('ij,ji->', A, B)
 
-    dot = np.zeros(rep)
-    dotC = np.zeros(rep)
-
-    for k in range(rep):
-        dot[k] = np.dot(u[k,:], A @ v[k,:].T) / (np.linalg.norm(u[k,:]) * np.linalg.norm(A @ v[k,:].T))        
-        dotC[k] = np.dot(u[k,:],v[k,:]) / (np.linalg.norm(u[k,:]) * np.linalg.norm(v[k,:]))
-
-    return dot,dotC
-
-def isotropic_normal(A,rep=100):
-    """
-    Given a square matrix A, calculate the symmetric (S) and skew-symmetric (N) scores of a matrix.
-    Args:
-        - A (numpy.ndarray) : square numpy matrix.
-
-    Returns:
-        - tuple : Symmetric (S) and skew-symmetric (N) scores.
-    """
-
-    u = np.random.rand(rep,A.shape[0])
-    dot = np.zeros(rep)
-    dotC = np.zeros(rep)
-    
-    for k in range(rep):
-        dot[k] = np.dot(u[k,:], A @ u[k,:].T) / (np.linalg.norm(u[k,:]) * np.linalg.norm(A @ u[k,:].T))        
-        dotC[k] = np.dot(u[k,:],u[k,:]) / (np.linalg.norm(u[k,:])**2)
-
-    return dot,dotC
+    return norm
