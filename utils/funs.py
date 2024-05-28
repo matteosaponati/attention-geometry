745a1cadeb262add0813450b2df6b3054fbc64e8
matteosaponati <matteosaponati@gmail.com>
2024-05-28 10:51:16 +0200

    add funs to go through nested attributes, and move symmetry scores funs in different file

--- utils/funs.py
+++ utils/funs.py
@@ -1,45 +1,13 @@
-import numpy as np 
+def get_nested_attr(obj, attr_path): 
+    """ Recursively get nested attributes"""
 
-def scores(A):
-    """ Takes a square matrix A, computes its symmetric and skew symmetric components (time complexity O(d)),
-    computes the norm of SYM and SKEWSYM, and finally computes the ratio of the norms.
-
-    Args:
-        - A (numpy.ndarray): square numpy matrix.
-    Returns:
-        - tuple: Symmetric (S) and skew-symmetric (N) scores.
-    """
-
-    SYM = .5 * (A + A.T)
-    SKEWSYM = .5 * (A - A.T)
-     
-    S = np.linalg.norm(SYM, 'fro') / np.linalg.norm(A, 'fro')
-    N = np.linalg.norm(SKEWSYM, 'fro') / np.linalg.norm(A, 'fro')
-
-    return S, N
-
-def scores_trace(Wq, Wk):
-     
-    ## (k x n) @ (n x k) -> O(nk^2)
-    A = Wq.T @ Wq
-    B = Wk.T @ Wk
-    C = Wk.T @ Wq
-
-    S = .5 * (1 + (np.einsum('ij,ji->', C, C) / np.einsum('ij,ji->', A, B)))
-
-    return S
-
-def zetas(Wq, Wk):
-     
-    M = Wq.T @ Wk
-    idx = np.einsum('ij,ji->', M, M) 
-
-    return idx
-
-def frobenious_norm(Wq, Wk):
-     
-    A = Wq.T @ Wq
-    B = Wk.T @ Wk
-    norm = np.einsum('ij,ji->', A, B)
-
-    return norm
+    attrs = attr_path.split('.')
+    
+    for attr in attrs:
+        if '[' in attr and ']' in attr:
+            attr_name, index = attr[: -1].split('[')
+            obj = getattr(obj, attr_name)[int(index)]
+        else:
+            obj = getattr(obj, attr)
+    
+    return obj
