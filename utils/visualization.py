a6074fe3888566f96c69c5c921b5cc6b216874df
matteosaponati <matteosaponati@gmail.com>
2024-05-31 10:39:10 +0200

    add visualization.py file to store visualization funs

---
+++ utils/visualization.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from .funs import count_outliers
+
+def symmetry_score_boxplot(models):
+
+    SList = [models[key][-1].flatten() for key in list(models.keys())]
+    names = list(models.keys())
+
+    plt.figure(figsize=(5,4))
+
+    ## BERT models
+    plt.boxplot(SList)
+
+    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+    plt.ylabel('symmetry scores')
+    plt.xticks(np.arange(1,len(names)+1),names)
+    plt.xticks(rotation=50)
+    plt.ylim(0,1)
+    plt.axhline(y=.5,color='k',linestyle='dashed')
+
+    return
+
+def symmetry_score_scatter(models):
+
+    SList = [models[key][-1].flatten() for key in list(models.keys())]
+    names = list(models.keys())
+
+    plt.figure(figsize=(4,4))
+
+    for i, model in enumerate(SList):
+        
+        plt.scatter(i*np.ones(SList[i].size),(SList[i].flatten()),color='purple',alpha=.1)
+        plt.scatter(i,(SList[i].flatten()).mean(),color='purple')
+
+    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+    plt.ylabel('symmetry scores')
+    plt.xticks(np.arange(len(names)),names)
+    plt.xticks(rotation=50)
+    plt.ylim(0,1)
+    plt.axhline(y=.5,color='k',linestyle='dashed')
+
+    return
+
+def symmetry_score_outliers(models):
+
+    SList = [models[key][-1].flatten() for key in list(models.keys())]
+    names = list(models.keys())
+
+    plt.figure(figsize=(5,4))
+
+    for i, model in enumerate(SList):
+        
+        outliers = count_outliers(SList[i].flatten())
+        plt.scatter(i, outliers[outliers < .5].sum() / SList[i].size, color='darkblue')
+        plt.scatter(i, outliers[outliers > .5].sum() / SList[i].size, color='purple')
+
+    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
+    plt.ylabel('# outliers')
+    plt.xticks(np.arange(len(names)),names)
+    plt.xticks(rotation = 50)
+    plt.axhline(y = .0,color='k',linestyle='dashed')
+
+    return
