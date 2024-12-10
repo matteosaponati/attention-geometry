0ff7ed4b36b51e48602e5210888c9583a03ce45d
matteosaponati <matteosaponati@gmail.com>
2024-12-10 11:58:31 +0100

    update visualization funs

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,112 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from .funs import count_outliers
 
-def symmetry_score_boxplot(models):
+def plot_median_scores(models, color = 'purple', mode = 'symmetry', alpha = 1.):
+    """
+    plots median scores with error bars for models based on symmetry or directionality
 
-    SList = [models[key][-1].flatten() for key in list(models.keys())]
-    names = list(models.keys())
+    parameters:
+        models: dictionary containing model data, including scores and parameter counts
+        color: string representing the color for the plot, default is 'purple'
+        mode: string indicating the type of scores to plot ('symmetry' or 'directionality'), default is 'symmetry'
+        alpha: float controlling the transparency of the plotted points, default is 1.0
 
-    plt.figure(figsize=(5,4))
+    returns:
+        none (displays a plot showing median scores with error bars for each model)
+    """
 
-    ## BERT models
-    plt.boxplot(SList)
-
-    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
-    plt.ylabel('symmetry scores')
-    plt.xticks(np.arange(1,len(names)+1),names)
-    plt.xticks(rotation=50)
-    plt.ylim(0,1)
-    plt.axhline(y=.5,color='k',linestyle='dashed')
-
-    return
-
-def symmetry_score_scatter(models):
-
-    SList = [models[key][-1].flatten() for key in list(models.keys())]
-    names = list(models.keys())
-
-    plt.figure(figsize=(4,4))
-
-    for i, model in enumerate(SList):
-        
-        plt.scatter(i*np.ones(SList[i].size),(SList[i].flatten()),color='purple',alpha=.1)
-        plt.scatter(i,(SList[i].flatten()).mean(),color='purple')
-
-    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
-    plt.ylabel('symmetry scores')
-    plt.xticks(np.arange(len(names)),names)
-    plt.xticks(rotation=50)
-    plt.ylim(0,1)
-    plt.axhline(y=.5,color='k',linestyle='dashed')
-
-    return
-
-def symmetry_score_outliers(models):
-
-    SList = [models[key][-1].flatten() for key in list(models.keys())]
-    names = list(models.keys())
-
-    plt.figure(figsize=(5,4))
-
-    for i, model in enumerate(SList):
-        
-        outliers = count_outliers(SList[i].flatten())
-        plt.scatter(i, outliers[outliers < .5].sum() / SList[i].size, color='darkblue')
-        plt.scatter(i, outliers[outliers > .5].sum() / SList[i].size, color='purple')
-
-    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
-    plt.ylabel('# outliers')
-    plt.xticks(np.arange(len(names)),names)
-    plt.xticks(rotation = 50)
-    plt.axhline(y = .0,color='k',linestyle='dashed')
-
-    return
-
-def plot_median_errorbars(parameters, scores, color = 'purple', alpha = 1.):
+    parameters = [models[key][-1][-1] for key in list(models.keys())]    
+    if mode == 'symmetry':  
+        scores = [2 * models[key][-3].flatten() - 1 for key in list(models.keys())]
+    if mode == 'directionality':
+        scores = [-1 * models[key][-2].flatten() for key in list(models.keys())]
 
     for k in range(len(parameters)):
 
-        scores_norm = 2 * scores[k] - 1
-
-        median = np.median(scores_norm)
-        q1_range = median - np.percentile(scores_norm, 25)
-        q2_range = np.percentile(scores_norm, 75) - median
+        median = np.median(scores[k])
+        q1_range = median - np.percentile(scores[k], 25)
+        q2_range = np.percentile(scores[k], 75) - median
 
         plt.errorbar(parameters[k], median, yerr = [[q1_range], [q2_range]], 
-                     fmt = 'o', ecolor = color, capsize = 5, marker = 'o', markersize = 5, color = color, alpha = alpha)
+                    fmt = 'o', ecolor = color, capsize = 5, marker = 'o', 
+                    markersize = 5, color = color, alpha = alpha)
+            
+# def plot_median_training(specs, models, colors = []):
 
-def plot_median_training(specs, models, colors = []):
+#     for idx, key in enumerate(list(specs.keys())):
 
-    for idx, key in enumerate(list(specs.keys())):
-
-        scores = 2 * models[key] - 1
-        plt.plot(specs[key][0], np.median(scores, axis = (0,1)), color = colors[idx])
-        plt.fill_between(specs[key][0], 
-                np.percentile(scores, 25, axis = (0,1)),
-                np.percentile(scores, 75, axis = (0,1)),
-                color = colors[idx], alpha = .3)
+#         scores = 2 * models[key] - 1
+#         plt.plot(specs[key][0], np.median(scores, axis = (0,1)), color = colors[idx])
+#         plt.fill_between(specs[key][0], 
+#                 np.percentile(scores, 25, axis = (0,1)),
+#                 np.percentile(scores, 75, axis = (0,1)),
+#                 color = colors[idx], alpha = .3)
         
-def plot_median_initialization(data, layers, heads, c = 'navy', mode = 'full'):
+# def plot_median_initialization(data, layers, heads, c = 'navy', mode = 'full'):
 
-    scores = np.zeros((layers, heads, len(data['_step'])))
-    for l in range(layers):
-            for h in range(heads):
-                scores[l, h, :] = 2 * data[f'Layer {l}/Head {h} Symmetry WqWk'] - 1
+#     scores = np.zeros((layers, heads, len(data['_step'])))
+#     for l in range(layers):
+#             for h in range(heads):
+#                 scores[l, h, :] = 2 * data[f'Layer {l}/Head {h} Symmetry WqWk'] - 1
 
-    if mode == 'full':
+#     if mode == 'full':
 
-        plt.plot(data['_step'], np.median(scores, axis = (0,1)), color = c)
-        plt.fill_between(data['_step'], 
-                np.percentile(scores, 25, axis = (0,1)),
-                np.percentile(scores, 75, axis = (0,1)),
-                color = c, alpha = .3)
+#         plt.plot(data['_step'], np.median(scores, axis = (0,1)), color = c)
+#         plt.fill_between(data['_step'], 
+#                 np.percentile(scores, 25, axis = (0,1)),
+#                 np.percentile(scores, 75, axis = (0,1)),
+#                 color = c, alpha = .3)
     
-    if mode == 'layers':
+#     if mode == 'layers':
 
-        for l in range(layers):
-            plt.plot(data['_step'], np.median(scores, axis = 1)[l, :], color = c[l], label = f'layer {l}')
-            plt.fill_between(data['_step'], 
-                        np.percentile(scores, 25, axis = 1)[l, :],
-                        np.percentile(scores, 75, axis = 1)[l, :],
-                        color = c[l], alpha = .3)
+#         for l in range(layers):
+#             plt.plot(data['_step'], np.median(scores, axis = 1)[l, :], color = c[l], label = f'layer {l}')
+#             plt.fill_between(data['_step'], 
+#                         np.percentile(scores, 25, axis = 1)[l, :],
+#                         np.percentile(scores, 75, axis = 1)[l, :],
+#                         color = c[l], alpha = .3)
