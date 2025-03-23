f88f8746b82444435e05fe4a169f101fdf3eed44
Pascal Sager <paes.sager@gmail.com>
2025-03-23 09:43:39 +0100

    Test dist setting

--- custom-models-training/test_dist.py
+++ custom-models-training/test_dist.py
@@ -2,11 +2,25 @@ import datetime
 import torch
 import os
 
-rank = int(os.environ.get("RANK", 0))
 
-print("Hello from rank", rank)
-torch.distributed.init_process_group(backend='nccl', timeout=datetime.timedelta(seconds=30))
-print("Connected rank", rank)
-torch.distributed.barrier()
-print("Barrier passed rank", rank)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local-rank",
+                        type=int,
+                        default=0,
+                        dest='rank',
+                        )
+
+    args = parser.parse_args()
+
+
+    rank = int(os.environ.get("RANK", 0))
+    print("Hello from rank", rank)
+    torch.distributed.init_process_group(backend='nccl', timeout=datetime.timedelta(seconds=30))
+    print("Connected rank", rank)
+    torch.distributed.barrier()
+    print("Barrier passed rank", rank)
 
